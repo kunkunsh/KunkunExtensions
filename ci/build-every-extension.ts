78141d76ae407418ea598c32ded4c66bb8e59e60
Huakun Shen <huakun.shen@huakunshen.com>
2024-08-26 02:00:30 -0400

    ci: add extension verify in ci

--- ci/build-every-extension.ts
+++ ci/build-every-extension.ts
@@ -5,12 +5,14 @@ import fs from "fs";
 
 for (const dir of fs.readdirSync(ExtensionsDir)) {
   const extPath = path.resolve(ExtensionsDir, dir);
-  const nodeModulesPath = path.join(extPath, "node_modules");
-  if (fs.existsSync(nodeModulesPath)) {
-    await $`rm -rf ${nodeModulesPath}`;
-  }
+  // const nodeModulesPath = path.join(extPath, "node_modules");
+  // if (fs.existsSync(nodeModulesPath)) {
+  //   await $`rm -rf ${nodeModulesPath}`;
+  // }
   // skip if not a directory
   if (!fs.statSync(extPath).isDirectory()) continue;
+  // verfiy extension with latest @kksh/cli
+  await $`npx @kksh/cli@latest verify ${extPath}`
   await $`pnpm install`.cwd(extPath);
   await $`pnpm build`.cwd(extPath);
 }
