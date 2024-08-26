3db343890380069018314ffaa9ab3c4e0a6350a1
Huakun Shen <huakun.shen@huakunshen.com>
2024-08-26 01:45:56 -0400

    ci: fix CI, remove pnpm-workspace from root.

--- ci/build-every-extension.ts
+++ ci/build-every-extension.ts
@@ -5,6 +5,10 @@ import fs from "fs";
 
 for (const dir of fs.readdirSync(ExtensionsDir)) {
   const extPath = path.resolve(ExtensionsDir, dir);
+  const nodeModulesPath = path.join(extPath, "node_modules");
+  if (fs.existsSync(nodeModulesPath)) {
+    await $`rm -rf ${nodeModulesPath}`;
+  }
   // skip if not a directory
   if (!fs.statSync(extPath).isDirectory()) continue;
   await $`pnpm install`.cwd(extPath);
