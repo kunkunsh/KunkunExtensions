7d94897118aefa8da976e18b18cd007793dfba7c
Huakun Shen <huakun.shen@huakunshen.com>
2024-08-28 20:37:53 -0400

    fix: fix vscode extension permission

--- extensions/vscode/index.ts
+++ extensions/vscode/index.ts
@@ -1,104 +1,137 @@
 import {
-	expose,
-	fs,
-	Icon,
-	IconEnum,
-	List,
-	os,
-	path,
-	shell,
-	toast,
-	ui,
-	WorkerExtension
-} from "@kksh/api/ui/worker"
-import { array, boolean, flatten, object, safeParse, string, type InferOutput } from "valibot"
+  expose,
+  fs,
+  Icon,
+  IconEnum,
+  List,
+  log,
+  os,
+  path,
+  shell,
+  toast,
+  ui,
+  WorkerExtension,
+} from "@kksh/api/ui/worker";
+import {
+  array,
+  boolean,
+  flatten,
+  object,
+  safeParse,
+  string,
+  type InferOutput,
+} from "valibot";
 
 const Project = object({
-	enabled: boolean(),
-	name: string(),
-	paths: array(string()),
-	rootPath: string(),
-	tags: array(string())
-})
-type Project = InferOutput<typeof Project>
-const Projects = array(Project)
-type Projects = InferOutput<typeof Projects>
+  enabled: boolean(),
+  name: string(),
+  paths: array(string()),
+  rootPath: string(),
+  tags: array(string()),
+});
+type Project = InferOutput<typeof Project>;
+const Projects = array(Project);
+type Projects = InferOutput<typeof Projects>;
 
 function mapProjectToItem(project: Project): List.Item {
-	return new List.Item({
-		title: project.name,
-		subTitle: project.rootPath,
-		value: project.rootPath,
-		icon: new Icon({ type: IconEnum.Iconify, value: "ri:folder-open-fill" })
-	})
+  return new List.Item({
+    title: project.name,
+    subTitle: project.rootPath,
+    value: project.rootPath,
+    icon: new Icon({ type: IconEnum.Iconify, value: "ri:folder-open-fill" }),
+  });
+}
+
+function openWithVSCode(path: string) {
+  return shell.Command.create("code", [path])
+    .execute()
+    .then((res) => {
+      toast.success(`Opened with VSCode`);
+    })
+    .catch((err) => {
+      toast.error(`Failed to open with VSCode: ${err}`);
+    });
 }
 
 class VSCodeProjectManager extends WorkerExtension {
-	async load() {
-		const platform = await os.platform()
-		let fileContent: string | undefined
-		if (platform === "macos") {
-			fileContent = await fs.readTextFile(
-				"Library/Application Support/Code/User/globalStorage/alefragnani.project-manager/projects.json",
-				{ baseDir: path.BaseDirectory.Home }
-			)
-		} else if (platform === "windows") {
-			fileContent = await fs.readTextFile(
-				"Code/User/globalStorage/alefragnani.project-manager/projects.json",
-				{ baseDir: path.BaseDirectory.AppData }
-			)
-		} else if (platform === "linux") {
-			fileContent = await fs.readTextFile(
-				".config/Code/User/globalStorage/alefragnani.project-manager/projects.json",
-				{ baseDir: path.BaseDirectory.Home }
-			)
-		} else {
-			return toast.error(`Unsupported platform: ${platform}`).then(() => Promise.resolve())
-		}
-		const parseRes = safeParse(Projects, JSON.parse(fileContent))
-		if (!parseRes.success) {
-			return toast
-				.error(`Failed to parse projects file: ${flatten<typeof Projects>(parseRes.issues)}`)
-				.then(() => Promise.resolve())
-		}
-		const projects = parseRes.output
+  async load() {
+    const platform = await os.platform();
+    let fileContent: string | undefined;
+    if (platform === "macos") {
+      fileContent = await fs.readTextFile(
+        "Library/Application Support/Code/User/globalStorage/alefragnani.project-manager/projects.json",
+        { baseDir: path.BaseDirectory.Home }
+      );
+    } else if (platform === "windows") {
+      fileContent = await fs.readTextFile(
+        "Code/User/globalStorage/alefragnani.project-manager/projects.json",
+        { baseDir: path.BaseDirectory.AppData }
+      );
+    } else if (platform === "linux") {
+      fileContent = await fs.readTextFile(
+        ".config/Code/User/globalStorage/alefragnani.project-manager/projects.json",
+        { baseDir: path.BaseDirectory.Home }
+      );
+    } else {
+      return toast
+        .error(`Unsupported platform: ${platform}`)
+        .then(() => Promise.resolve());
+    }
+    const parseRes = safeParse(Projects, JSON.parse(fileContent));
+    if (!parseRes.success) {
+      return toast
+        .error(
+          `Failed to parse projects file: ${flatten<typeof Projects>(parseRes.issues)}`
+        )
+        .then(() => Promise.resolve());
+    }
+    const projects = parseRes.output;
 
-		const tags = projects.map((project) => project.tags).flat()
-		const sections = tags.map((tag) => {
-			return new List.Section({
-				title: tag,
-				items: projects.filter((project) => project.tags.includes(tag)).map(mapProjectToItem)
-			})
-		})
-		sections.push(
-			new List.Section({
-				title: "[no tags]",
-				items: projects.filter((project) => project.tags.length === 0).map(mapProjectToItem)
-			})
-		)
+    const tags = projects.map((project) => project.tags).flat();
+    const sections = tags.map((tag) => {
+      return new List.Section({
+        title: tag,
+        items: projects
+          .filter((project) => project.tags.includes(tag))
+          .map(mapProjectToItem),
+      });
+    });
+    sections.push(
+      new List.Section({
+        title: "[no tags]",
+        items: projects
+          .filter((project) => project.tags.length === 0)
+          .map(mapProjectToItem),
+      })
+    );
 
-		return ui.setSearchBarPlaceholder("Search for projects...").then(() => {
-			return ui.render(new List.List({ sections }))
-		})
-	}
+    return ui.setSearchBarPlaceholder("Search for projects...").then(() => {
+      return ui.render(new List.List({ sections }));
+    });
+  }
 
-	onSearchTermChange(term: string): Promise<void> {
-		console.log("Search term changed to:", term)
-		return Promise.resolve()
-	}
+  onSearchTermChange(term: string): Promise<void> {
+    console.log("Search term changed to:", term);
+    return Promise.resolve();
+  }
 
-	async onListItemSelected(value: string): Promise<void> {
-		const platform = await os.platform()
-		if (platform === "macos") {
-			shell.executeBashScript(`open -a "Visual Studio Code" "${value}"`)
-		} else if (platform === "windows") {
-			// TODO
-		} else if (platform === "linux") {
-			// TODO
-		} else {
-			toast.error(`Unsupported platform: ${platform}).then(() => Promise.resolve()`)
-		}
-	}
+  async onListItemSelected(value: string): Promise<void> {
+    log.info(`Selected project: ${value}`);
+    const platform = await os.platform();
+    if (platform === "macos") {
+      openWithVSCode(value);
+      //   shell.Command.create("code", [value]).execute();
+      // shell.executeBashScript(`open -a "Visual Studio Code" "${value}"`)
+    } else if (platform === "windows") {
+      openWithVSCode(value);
+    } else if (platform === "linux") {
+      openWithVSCode(value);
+    } else {
+      toast.error(
+        `Unsupported platform: ${platform}).then(() => Promise.resolve()`
+      );
+    }
+  }
 }
 
-expose(new VSCodeProjectManager())
+expose(new VSCodeProjectManager());
