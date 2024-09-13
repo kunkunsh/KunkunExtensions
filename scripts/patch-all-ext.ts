dc209ac535ba5727e36c117b3cc87063608bf6bf
Huakun Shen <huakun.shen@huakunshen.com>
2024-09-13 00:19:48 -0400

    Add patch-all-ext.ts

---
+++ scripts/patch-all-ext.ts
@@ -0,0 +1,19 @@
+import {inc} from 'semver'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import fs from 'fs'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const EXTENSIONS_DIR = path.resolve(__dirname, "../extensions")
+
+for (const ext of fs.readdirSync(EXTENSIONS_DIR)) {
+	const extPath = path.resolve(EXTENSIONS_DIR, ext)
+	const pkgJsonPath = path.resolve(extPath, "package.json")
+	if (!fs.existsSync(pkgJsonPath)) {
+		console.log(`${extPath} is not a valid extension`)
+		continue
+	}
+	const pkgJson = JSON.parse(fs.readFileSync(pkgJsonPath, "utf-8"))
+	pkgJson.version = inc(pkgJson.version, 'patch')
+	fs.writeFileSync(pkgJsonPath, JSON.stringify(pkgJson, null, "\t"))
+}
