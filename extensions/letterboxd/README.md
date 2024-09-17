7eab90f160557e309fd1e56d64d99909fd54bebb
Jacky Lai <jlai@venacorp.com>
2024-09-16 21:25:29 -0400

    Create Letterboxd Plugin for Kunkun

---
+++ extensions/letterboxd/README.md
@@ -0,0 +1,15 @@
+# Letterboxd Plugin for Kunkun
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run index.ts
+```
+
+Search for movies on Letterboxd, add to watchlist, log a film...
---
+++ extensions/letterboxd/src/actions.ts
@@ -0,0 +1,61 @@
+export const generalActions = [
+    {
+        "title": "Add to Watchlist",
+        "value": "watchlist",
+        "icon": "tabler:clock-plus"
+    },
+    {
+        "title": "Log",
+        "value": "review",
+        "icon": "mdi:plus-circle-outline"
+    },
+    {
+        "title": "Change Poster",
+        "value": "poster",
+        "icon": "material-symbols:frame-reload-sharp"
+    },
+    {
+        "title": "Popular Reviews",
+        "value": "reviews/by/activity",
+        "icon": "fluent:people-team-28-regular"
+    },
+    {
+        "title": "Popular Lists",
+        "value": "lists/by/popular",
+        "icon": "ph:user-list"
+    },
+    {
+        "title": "Where to Watch",
+        "value": "watch",
+        "icon": "fluent:people-team-28-regular"
+    }
+]
+
+export const friendsActions = [
+    {
+        "title": "Reviews by Friends",
+        "value": "reviews",
+        "icon": "material-symbols:person-outline"
+    },
+    {
+        "title": "Lists by Friends",
+        "value": "lists",
+        "icon": "clarity:list-line"
+    }
+]
+
+export const userActions = [
+    {
+        "title": "My Activities",
+        "value": "activity",
+        "icon": "fluent:people-team-28-regular"
+    }
+]
+
+export const specialActions = [
+    {
+        "title": "Settings",
+        "value": "settings",
+        "icon": "ant-design:setting-outlined"
+    }
+]
